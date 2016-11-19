918f5f4c0085684372a321b138a93c6d2499c1b5
Benjamin Byholm <bbyholm@abo.fi>
2016-11-19 02:10:45 +0200

    Easier matching

--- src/main/scala/SimplyParser.scala
+++ src/main/scala/SimplyParser.scala
@@ -14,91 +14,91 @@ class SimplyParser extends RegexParsers with PackratParsers {
   def data: Parser[List[DATA_EXP]] = "Data" ~> rep(data_exp)
 
   def data_exp: Parser[DATA_EXP] = (
-    id ~ ":=" ~ arithm_exp <~ ";" ^^ { case ident ~ ":=" ~ exp => DATA_EXP(ident, exp) }
-  | id ~ ":=" ~ formula <~ ";" ^^ { case ident ~ ":=" ~ predicate => DATA_EXP(ident, predicate)}
+    id ~ (":=" ~> arithm_exp) <~ ";" ^^ { case ident ~ exp => DATA_EXP(ident, exp) }
+  | id ~ (":=" ~> formula) <~ ";" ^^ { case ident ~ predicate => DATA_EXP(ident, predicate)}
   )
 
   def domains: Parser[List[DOMAIN_EXP]] = "Domains" ~> rep(domain_exp)
 
-  def domain_exp: Parser[DOMAIN_EXP] = "Dom" ~> id ~ "=" ~ list <~ ";" ^^ { case ident ~ "=" ~ l => DOMAIN_EXP(ident, l) }
+  def domain_exp: Parser[DOMAIN_EXP] = "Dom" ~> id ~ ("=" ~> list) <~ ";" ^^ { case ident ~ l => DOMAIN_EXP(ident, l) }
 
   def variables: Parser[List[VARIABLE_EXP]] = "Variables" ~> rep(variable_exp)
 
   def variable_exp: Parser[VARIABLE_EXP] = (
-    "IntVar" ~> rep1sep(var_id, ",") ~ "::" ~ id <~ ";" ^^ { case vars ~ "::" ~ ident => VARIABLE_EXP(vars, ident) }
-  | "BoolVar" ~> rep1sep(var_id, ",") <~ ";" ^^ { x => VARIABLE_EXP(x, IDENTIFIER("__BOOL__"))}
+    "IntVar" ~> rep1sep(var_id, ",") ~ ("::" ~> id) <~ ";" ^^ { case vars ~ ident => VARIABLE_EXP(vars, ident) }
+  | "BoolVar" ~> rep1sep(var_id, ",") <~ ";" ^^ { VARIABLE_EXP(_, IDENTIFIER("__BOOL__"))}
   )
 
   def var_id: Parser[VAR_ID] = (
-    id ~ "[" ~ rep1sep(arithm_exp, ",") <~ "]" ^^ { case id ~ "[" ~ (list : List[ARITHM_EXP]) => VAR_ID(id, list)}
-  | id ^^ { x => VAR_ID(x) }
+    id ~ ("[" ~> rep1sep(arithm_exp, ",")) <~ "]" ^^ { case id ~ list => VAR_ID(id, list)}
+  | id ^^ { VAR_ID(_) }
   )
 
   def constraints: Parser[List[SENTENCE]] = "Constraints" ~> rep1(sentence)
 
   def sentence: Parser[SENTENCE] = (
-    statement ^^ { x => SENTENCE_STATEMENT(x) }
-  | constraint <~ ";" ^^ { x => SENTENCE_CONSTRAINT(x) }
+    statement ^^ { SENTENCE_STATEMENT }
+  | constraint <~ ";" ^^ { SENTENCE_CONSTRAINT }
   )
 
   def statement: Parser[STATEMENT] = (
-    if_then_else ^^ { x => IF_THEN_ELSE_STATEMENT(x) }
-  | forall ^^ { x => FORALL_STATEMENT(x) }
+    if_then_else ^^ { IF_THEN_ELSE_STATEMENT }
+  | forall ^^ { FORALL_STATEMENT }
   )
 
   def if_then_else: Parser[IF_THEN_ELSE] = (
-    "If" ~> "(" ~> formula ~ ")" ~ "Then" ~ "{" ~ rep1(sentence) <~ "}" ^^ { case f ~ ")" ~ "Then" ~ "{" ~ ifs => IF_THEN_ELSE(f, ifs) }
-  | "If" ~> "(" ~> formula ~ ")" ~ "Then" ~ "{" ~ rep1(sentence) ~ "}" ~ "Else" ~ "{" ~ rep1(sentence) <~ "}" ^^ { case f ~ ")" ~ "Then" ~ "{" ~ ifs ~ "}" ~ "Else" ~ "{" ~ elses => IF_THEN_ELSE(f, ifs, elses) }
+    "If" ~> "(" ~> formula ~ (")" ~> "Then" ~> "{" ~> rep1(sentence)) <~ "}" ^^ { case f ~ ifs => IF_THEN_ELSE(f, ifs) }
+  | "If" ~> "(" ~> formula ~ (")" ~> "Then" ~> "{" ~> rep1(sentence)) ~ ("}" ~> "Else" ~> "{" ~> rep1(sentence)) <~ "}" ^^ { case f ~ ifs ~ elses => IF_THEN_ELSE(f, ifs, elses) }
   )
 
-  def forall: Parser[FORALL] = "Forall" ~> "(" ~> id ~ "in" ~ list ~ ")" ~ "{" ~ rep1(sentence) <~ "}" ^^ { case ident ~ "in" ~ l ~ ")" ~ "{" ~ sentences => FORALL(ident, l, sentences) }
+  def forall: Parser[FORALL] = "Forall" ~> "(" ~> id ~ ("in" ~> list) ~ (")" ~> "{" ~> rep1(sentence)) <~ "}" ^^ { case ident ~ l ~ sentences => FORALL(ident, l, sentences) }
 
   def constraint: Parser[CONSTRAINT] = (
     global_constraint
-  | "If_Then_Else" ~> "(" ~> formula ~ ")" ~ "{" ~ rep1(sentence) ~ "}" ~ "{" ~ rep1(sentence) <~ "}" ^^ { case f ~ ")" ~ "{" ~ ifs ~ "}" ~ "{" ~ elses => IF_THEN_ELSE_CONSTRAINT(f, ifs, elses) }
-  | formula ^^ { x => PREDICATE_CONSTRAINT(x) }
+  | "If_Then_Else" ~> "(" ~> formula ~ (")" ~> "{" ~> rep1(sentence)) ~ ("}" ~> "{" ~> rep1(sentence)) <~ "}" ^^ { case f ~ ifs ~ elses => IF_THEN_ELSE_CONSTRAINT(f, ifs, elses) }
+  | formula ^^ { PREDICATE_CONSTRAINT }
   )
 
   lazy val formula: PackratParser[FORMULA] = (
-    "Not" ~> formula ^^ { x => NOT_FORMULA(x) }
+    "Not" ~> formula ^^ { NOT_FORMULA }
   | formula ~ bool_operator ~ formula ^^ { case lhs ~ op ~ rhs => BOOL_OP_FORMULA(op, lhs, rhs) }
   | arithm_exp ~ relational_operator ~ arithm_exp ^^ { case lhs ~ op ~ rhs  => REL_OP_FORMULA(op, lhs, rhs) }
   | "(" ~> formula <~ ")"
-  | var_id ^^ { x => VAR_FORMULA(x) }
+  | var_id ^^ { VAR_FORMULA }
   | "True" ^^^ { CONST_FORMULA(true) }
   | "False" ^^^ { CONST_FORMULA(false) }
   )
 
   def global_constraint: Parser[GLOBAL_CONSTRAINT] = (
-    "AllDifferent" ~> "(" ~> list <~ ")" ^^ { x => ALLDIFFERENT_CONSTRAINT(x) }
-  | "Sum" ~> "(" ~> list ~ "," ~ arithm_exp <~ ")" ^^ { case l ~ "," ~ exp => SUM_CONSTRAINT(l, exp) }
-  | "Count" ~> "(" ~> list ~ "," ~ arithm_exp ~ "," ~ arithm_exp <~ ")" ^^ { case l ~ "," ~ value ~ "," ~ count => COUNT_CONSTRAINT(l, value, count) }
+    "AllDifferent" ~> "(" ~> list <~ ")" ^^ { ALLDIFFERENT_CONSTRAINT }
+  | "Sum" ~> "(" ~> list ~ ("," ~> arithm_exp) <~ ")" ^^ { case l ~ exp => SUM_CONSTRAINT(l, exp) }
+  | "Count" ~> "(" ~> list ~ ("," ~> arithm_exp) ~ ("," ~> arithm_exp) <~ ")" ^^ { case l ~ value ~ count => COUNT_CONSTRAINT(l, value, count) }
   )
 
   lazy val arithm_exp: PackratParser[ARITHM_EXP] = (
     arithm_exp ~ arithm_operator ~ arithm_exp ^^ { case lhs ~ op ~ rhs => ARITHM_OP_EXP(op, lhs, rhs) }
-    | "Abs" ~> "(" ~> arithm_exp <~ ")" ^^ { x => ABS_EXP(x) }
-    | numeral ^^ { x => CONST_EXP(x) }
-    | var_id ^^ { x => VAR_EXP(x) }
+    | "Abs" ~> "(" ~> arithm_exp <~ ")" ^^ { ABS_EXP }
+    | numeral ^^ { CONST_EXP }
+    | var_id ^^ { VAR_EXP }
     | "(" ~> arithm_exp <~ ")"
   )
 
   def list: Parser[LIST] = (
-    "[" ~> rep1sep(list_element, ",") <~ "]" ^^ { x => LIST_ENUMERATION(x) }
-  | "[" ~> arithm_exp ~ "|" ~ rep1sep(var_restrict, ",") <~ "]" ^^ { case exp ~ "|" ~ r => LIST_COMPREHENSION(exp, r) }
+    "[" ~> rep1sep(list_element, ",") <~ "]" ^^ { LIST_ENUMERATION }
+  | "[" ~> arithm_exp ~ ("|" ~> rep1sep(var_restrict, ",")) <~ "]" ^^ { case exp ~ r => LIST_COMPREHENSION(exp, r) }
   )
 
   def list_element: Parser[LIST_ELEMENT] = (
-    range ^^ { x => LIST_ELEMENT_RANGE(x) }
-  | arithm_exp ^^ { x => LIST_ELEMENT_EXP(x) }
+    range ^^ { LIST_ELEMENT_RANGE }
+  | arithm_exp ^^ { LIST_ELEMENT_EXP }
   )
 
   def var_restrict: Parser[VAR_RESTRICT] = (
-    id ~ "in" ~ list ^^ { case ident ~ "in" ~ l => MEMBER_RESTRICT(ident, l) }
-  | formula ^^ { x => PREDICATE_RESTRICT(x) }
+    id ~ ("in" ~> list) ^^ { case ident ~ l => MEMBER_RESTRICT(ident, l) }
+  | formula ^^ { PREDICATE_RESTRICT }
   )
 
-  def range: Parser[RANGE] = arithm_exp ~ ".." ~ arithm_exp ^^ { case lb ~ ".." ~ ub => RANGE(lb, ub) }
+  def range: Parser[RANGE] = arithm_exp ~ (".." ~> arithm_exp) ^^ { case lb ~ ub => RANGE(lb, ub) }
 
   def bool_operator: Parser[String] = (
     "And"
@@ -125,9 +125,9 @@ class SimplyParser extends RegexParsers with PackratParsers {
   | "Mod"
   )
 
-  def id: Parser[IDENTIFIER] = """[A-Za-z_]\w*""".r ^^ { x => IDENTIFIER(x.toString)}
+  def id: Parser[IDENTIFIER] = """[A-Za-z_]\w*""".r ^^ { IDENTIFIER }
 
-  def numeral: Parser[NUMERAL] = """\d+""".r ^^ { x => NUMERAL(x.toInt) }
+  def numeral: Parser[NUMERAL] = """\d+""".r ^^ { string => NUMERAL(string.toInt) }
 }
 
 object SimplyParserTest extends SimplyParser {
