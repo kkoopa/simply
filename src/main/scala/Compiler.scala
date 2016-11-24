3dc5136a1cebd1c9694ae56a19632431d33075be
Benjamin Byholm <bbyholm@abo.fi>
2016-11-25 01:59:49 +0200

    Join all predicate constraints. simplify and split conjunctions into multiple constraints

--- src/main/scala/Compiler.scala
+++ src/main/scala/Compiler.scala
@@ -10,15 +10,23 @@ object Compiler {
 
   def printAST(path: String): Unit = println(Parser.parseAll(Parser.simply_problem, new FileReader(path)).get.treeString)
 
+  def splitProduct(p : PREDICATE_CONSTRAINT) : List[PREDICATE_CONSTRAINT] = p match {
+    case PREDICATE_CONSTRAINT(BOOL_OP_FORMULA("And", lhs, rhs)) => splitProduct(PREDICATE_CONSTRAINT(lhs)) ++ splitProduct(PREDICATE_CONSTRAINT(rhs))
+    case _ => List(p)
+  }
+
   def compile(path : String): String = {
     Env.clear()
     val tree = Parser.parseAll(Parser.simply_problem, new FileReader(path)).get
     val consts = Optimizer.visitProblem(tree)
+    val (predConsts, globalConsts) = consts partition { case c:PREDICATE_CONSTRAINT => true; case _ => false}
+    val preds = predConsts.asInstanceOf[List[PREDICATE_CONSTRAINT]] map { case PREDICATE_CONSTRAINT(p) => p }
+    val pred = preds reduce { BOOL_OP_FORMULA("And", _, _) }
     var res = ""
     for ((name, (dimensions, domain)) <- Env.xev) {
       res = res ++ name ++ dimensions.flatMap("[" ++ _.toString ++ "]") ++ "::[" ++ domain.mkString(", ") ++ "]\n"
     }
-    res ++ (consts.map(Printer.pretty) mkString "\n") ++ "\n"
+    res ++ (splitProduct(PREDICATE_CONSTRAINT(pred.simplify)) ++ globalConsts map Printer.pretty mkString "\n") ++ "\n"
   }
 
   def main(args: Array[String]): Unit = {
